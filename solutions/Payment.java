4df71407db22a2e8156e61b0cc3a06f53d7503f9
Eric020615 <92671386+Eric020615@users.noreply.github.com>
2022-06-19 01:04:31 +0800

    Update Payment.java

--- solutions/Payment.java
+++ solutions/Payment.java
@@ -1,194 +1,75 @@
 package solutions;
 import java.util.*;
-import java.io.*;
-public class Payment {
-    public static class Transaction implements Comparable<Transaction> {
-        private long time;
-        private String id;
-        private String tier;
-
-        public Transaction(long time,String id,String tier){
-            this.time=time;
-            this.id=id;
-            this.tier=tier;
-        }
-
-        public long getTime() {
-            return time;
-        }
-
-        public String getId() {
-            return id;
-        }
-
-        public String getTier() {
-            return tier;
-        }
-
-        public void setTime(long time) {
-            this.time = time;
-        }
-
-        public void setId(String id) {
-            this.id = id;
-        }
-
-        public void setTier(String tier) {
-            this.tier = tier;
-        }
-
-        @Override
-        public String toString() {
-            return time+" "+id+" "+tier;
-        }
-
-        @Override
-        public int compareTo(Transaction o) {
-            if(this.getTime()>o.getTime())
-                return -1;
-            else if(this.getTime()<o.getTime())
-                return 1;
-            else
-                return 0;
-        }
-    }
-    
-    public static class MyQueue<E>  {
-        private java.util.LinkedList<E> list = new java.util.LinkedList<>();
-
-        public MyQueue(ArrayList e){
-            this.list=new LinkedList<E>(e);
-        }
-
-
-        public MyQueue(E[] e){
-            this.list=new LinkedList<E>(Arrays.asList(e));
-        }
-
-        public MyQueue(){
-            this.list=new LinkedList<E>();
-        }
-
-        public void enqueue(E e){
-            list.addLast(e);
-        }
-
-        public void enqueue(ArrayList e){
-            list.addLast((E) e);
-        }
-
-
-        public E dequeue(){
-            return list.removeFirst();
-        }
-
-        public E getElement(int i){
-            return list.get(i);
-        }
-
-        public E peek(){
-            return list.peek();
-        }
-
-        public int size(){
-            return list.size();
-        }
-
-        public E get(int i){
-            return list.get(i);
-        }
-
-        public boolean contains(E e){
-            return list.contains(e);
-        }
-
-        public boolean isEmpty(){
-            return list.isEmpty();
-        }
-
-        public E poll(){
-            E element = list.peek();
-            list.removeFirst();
-            return element;
-        }
-
-        public void set(int i, E element){
-            list.set(i, element);
-        }
-
-        public void clear(){
-            list.clear();
-        }
-        public void toPrintQueue(){
-            for(int i=0;i< list.size();i++) {
-                System.out.println((i+1)+" "+list.get(i));
-            }
-        }
-    }
-    
-    public static void main(String[] args) throws IOException{
-        long StartTime = new Date(). getTime(); //some tasks long lEndTime = new Date(). getTime(); long difference = lEndTime - lStartTime; System.
+public class payment_user_input {
+    public static void main(String[] args) {
         LinkedList<String>queue = new LinkedList<>();
         ArrayList<Transaction>input = new ArrayList<>();
-        File dir = new File("DS/task/payment/cases");
-        File[]files=dir.listFiles();
-
-        if (files[0].isFile()){
-            Scanner sc=null;
-            String word;
-
-            try{
-                sc=new Scanner(new FileInputStream(files[0]));
-                word=sc.nextLine();
-                String[] element = word.split(" ");
-                long time = Long.parseLong(element[0]);
-                Transaction t = new Transaction(time, element[1], element[2]);
-                input.add(t);
-
-                long startnum =(( time % 10000)/1000);
-
-                while(sc.hasNextLine()){
-                    word=sc.nextLine();
-                    element=word.split(" ");
-                    time =Long.parseLong(element[0]);
-                    Transaction t1 = new Transaction(time,element[1],element[2]);
-                    input.add(t1);
-
-                    long number =(( time % 10000)/1000);
-                    if(startnum!=number) {
-                        System.out.println("Arrange based on tier system :");
-                        setTimeBasedOnTier(input);
-                        Collections.sort(input);
-                        printTrans(input);
-                        for (int i=0;i<input.size();i++){
-                            queue.add(input.get(i).getId());
-                        }
-                        input.clear();
-                        System.out.println("Output:");
-                        for (int i=0;i<100;i++) {
-                            System.out.println((i+1)+")"+queue.removeFirst());
-                        }
-                    }
-                    startnum = number;
-                }
-            }catch(IOException e){
-                System.out.println("File got some problems.");
-            }finally {
-                if (sc!=null){
-                    sc.close();
-                }
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        while((word.equalsIgnoreCase("END"))||(word.equalsIgnoreCase("CLEAR"))) {
+            if (word.equalsIgnoreCase("END")) {
+                System.exit(0);
+            }else if (word.equalsIgnoreCase("CLEAR")) {
+                word = sc.nextLine();
             }
         }
-        long EndTime = new Date(). getTime();
-        long difference = EndTime - StartTime;
-        System.out.println("The execution time is " +difference+ "ms");
+        String[] element = word.split(" ");
+        long time = Long.parseLong(element[0]);
+        Transaction t = new Transaction(time, element[1], element[2]);
+        input.add(t);
+        long startnum = ((time % 10000) / 1000);
+        while(true) {
+            if (word.equalsIgnoreCase("END")) {
+                System.exit(0);
+            }
+            else{
+                word = sc.nextLine();
+                if (word.equalsIgnoreCase("CLEAR")) {
+                    queue.clear();
+                    continue;
+                }
+                element = word.split(" ");
+                if (word.equalsIgnoreCase("END")) {
+                    System.exit(0);
+                }
+                time = Long.parseLong(element[0]);
+                Transaction t1 = new Transaction(time, element[1], element[2]);
+                input.add(t1);
 
+                long number = ((time % 10000) / 1000);
+                if(startnum!=number) {
+                    System.out.println("Arrange based on tier system :");
+                    setTimeBasedOnTier(input);
+                    Collections.sort(input);
+                    printTrans(input);
+                    for (int i=0;i<input.size();i++){
+                        queue.add(input.get(i).getId());
+                    }
+                    input.clear();
+                    System.out.print("Output:");
+                    for (int i=0;i<100;i++) {
+                        System.out.println(queue.removeFirst());
+                    }
+                }
+                startnum = number;
+            }
+        }
     }
 
+
     public static void printTrans(ArrayList list){
-        // tem.out.println("The 100 transaction in the list :");
         for(Object e :list){
             System.out.println(e);
+        }
+    }
+
+    public static void printID(ArrayList list){
+        int i=0;
+        for(Object e :list){
+            if(i%10==0)
+                System.out.println("");
+            System.out.print(e+" ");
+            i++;
         }
     }
 
