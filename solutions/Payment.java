5d8a4ded39e70768002082f8f1587333c8d1dc23
Eric020615 <92671386+Eric020615@users.noreply.github.com>
2022-06-19 01:06:05 +0800

    Update Payment.java

--- solutions/Payment.java
+++ solutions/Payment.java
@@ -1,6 +1,59 @@
 package solutions;
 import java.util.*;
 public class payment_user_input {
+    
+    public static class Transaction implements Comparable<Transaction> {
+        private long time;
+        private String id;
+        private String tier;
+
+        public Transaction(long time,String id,String tier){
+            this.time=time;
+            this.id=id;
+            this.tier=tier;
+        }
+
+        public long getTime() {
+            return time;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getTier() {
+            return tier;
+        }
+
+        public void setTime(long time) {
+            this.time = time;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public void setTier(String tier) {
+            this.tier = tier;
+        }
+
+        @Override
+        public String toString() {
+            return time+" "+id+" "+tier;
+        }
+
+        @Override
+        public int compareTo(Transaction o) {
+            if(this.getTime()>o.getTime())
+                return -1;
+            else if(this.getTime()<o.getTime())
+                return 1;
+            else
+                return 0;
+        }
+
+    }
+    
     public static void main(String[] args) {
         LinkedList<String>queue = new LinkedList<>();
         ArrayList<Transaction>input = new ArrayList<>();
