9e412fb53ad6d1cf5615292f9e4aa263b8845e25
Eric020615 <92671386+Eric020615@users.noreply.github.com>
2022-06-21 22:52:06 +0800

    Update Payment.java

--- solutions/Payment.java
+++ solutions/Payment.java
@@ -1,58 +1,5 @@
 import java.util.*;
 public class Payment {
-    public static class Transaction implements Comparable<Transaction> {
-        private long time;
-        private String id;
-        private String tier;
-
-        public Transaction(long time,String id,String tier){
-            this.time=time;
-            this.id=id;
-            this.tier=tier;
-        }
-
-        public long getTime() {
-            return time;
-        }
-
-        public String getId() {
-            return id;
-        }
-
-        public String getTier() {
-            return tier;
-        }
-
-        public void setTime(long time) {
-            this.time = time;
-        }
-
-        public void setId(String id) {
-            this.id = id;
-        }
-
-        public void setTier(String tier) {
-            this.tier = tier;
-        }
-
-        @Override
-        public String toString() {
-            return time+" "+id+" "+tier;
-        }
-
-        @Override
-        public int compareTo(Payment.Transaction o) {
-            if(this.getTime()>o.getTime())
-                return -1;
-            else if(this.getTime()<o.getTime())
-                return 1;
-            else
-                return 0;
-        }
-    }
-    
-   
-    
     public static void main(String[] args) {
         LinkedList<String>queue = new LinkedList<>();
         ArrayList<Payment.Transaction>input = new ArrayList<>();
@@ -105,8 +52,6 @@ public class Payment {
             }
         }
     }
-
-
     public static void printTrans(ArrayList list){
         for(Object e :list){
             System.out.println(e);
@@ -141,6 +86,57 @@ public class Payment {
                 temp.setTime(change);
             }
 
+        }
+    }
+    
+    public static class Transaction implements Comparable<Transaction> {
+        private long time;
+        private String id;
+        private String tier;
+
+        public Transaction(long time,String id,String tier){
+            this.time=time;
+            this.id=id;
+            this.tier=tier;
+        }
+
+        public long getTime() {
+            return time;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getTier() {
+            return tier;
+        }
+
+        public void setTime(long time) {
+            this.time = time;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public void setTier(String tier) {
+            this.tier = tier;
+        }
+
+        @Override
+        public String toString() {
+            return time+" "+id+" "+tier;
+        }
+
+        @Override
+        public int compareTo(Payment.Transaction o) {
+            if(this.getTime()>o.getTime())
+                return -1;
+            else if(this.getTime()<o.getTime())
+                return 1;
+            else
+                return 0;
         }
     }
 }
